821cdf1bf7e4414d4ff400c79e84da2ff6ede4cf
Mikkel Andersen <normannmail@gmail.com>
2019-12-05 09:52:39 +0100

    quote fade

--- web/JS/javascript.js
+++ web/JS/javascript.js
@@ -23,4 +23,21 @@
     }
 	});
   //navbar scroll change color end
+
+  (function() {
+
+    var quotes = $(".quotes");
+    var quoteIndex = -1;
+
+    function showNextQuote() {
+        quoteIndex++;
+        quotes.eq(quoteIndex % quotes.length)
+            .fadeIn(2000)
+            .delay(2000)
+            .fadeOut(2000, showNextQuote);
+    }
+
+    showNextQuote();
+
+})();
  });
