c74e94b4340ba233451f4b0b0c584dda1d2b5fbe
Martin Brink <martin-e@live.dk>
2019-12-09 13:41:16 +0100

    Added fade slideing for case section

--- web/JS/javascript.js
+++ web/JS/javascript.js
@@ -35,9 +35,28 @@
             .fadeIn(2000)
             .delay(2000)
             .fadeOut(2000, showNextQuote);
+
     }
 
     showNextQuote();
 
 })();
- });
+
+(function() {
+
+  var quotes = $(".casecontent");
+  var quoteIndex = -1;
+
+  function showNextQuote() {
+      quoteIndex++;
+      quotes.eq(quoteIndex % quotes.length)
+          .fadeIn(0)
+          .delay(10000)
+          .fadeOut(2000, showNextQuote);
+  }
+
+  showNextQuote();
+
+})();
+
+});
